a92deee5894ac11f9bec31ca26b22dfa30e630f4
Brick <brickbradford@outlook.com>
2018-07-24 19:43:06 +1000

    updated readme

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#BASE_URL=http://anothertodo-tododo.193b.starter-ca-central-1.openshiftapps.com/test/1.0
 BASE_URL=http://localhost:8080/test/1.0
 
-curl -sSL -G -X GET "http://join.autogeneral.com.au/test/1.0/integrationTest" --data-urlencode "url=${BASE_URL}" -H  "accept: application/json" 
+curl -sSL -G -X GET "http://my.test.server/test/1.0/integrationTest" --data-urlencode "url=${BASE_URL}" -H  "accept: application/json" 
 
 
